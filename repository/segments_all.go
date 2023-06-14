18011483fb0278f09fc0550a62c59e094c645a02
omlight95 <onemorelight234@gmail.com>
2023-06-15 00:40:34 +0800

    upgrade go version

--- repository/segments_all.go
+++ repository/segments_all.go
@@ -3,13 +3,11 @@ package repository
 import (
 	"github.com/jenrain/OnlyID/entity"
 	"github.com/jenrain/OnlyID/library/log"
-	"github.com/jenrain/OnlyID/library/tool"
 	"go.uber.org/zap"
 )
 
-// SegmentsGetAll 只获取最近六小时的所有id
 func (r *Repository) SegmentsGetAll() (res []entity.Segments, err error) {
-	if err = r.db.Table("segments").Where("update_time >= ?", tool.GetTimeUnix()-21600).Find(&res).Error; err != nil {
+	if err = r.db.Table("segments").Find(&res).Error; err != nil {
 		log.GetLogger().Error("[Repository] SegmentGetAll Find", zap.Error(err))
 	}
 	return
